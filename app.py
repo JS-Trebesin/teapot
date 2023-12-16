62eba4588df7f99eaf6c3ad51163fadba56d750b
JS <souhrada@trebesin.cz>
2023-12-16 01:32:22 +0100

    cleaned up repo

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 from flask import Flask, render_template
-import time
 import ascii_arts as asciis
 
 
 app = Flask(__name__)
-
-# following line is just for dev purposes
-app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 coffee_done = False
 
 
 @app.route("/")
 def index():
-    # return render_template("test.html"), 418
     return render_template("teapot.html"), 418
 
 
