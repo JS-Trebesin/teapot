7ab45c50d5c652587bd93bb6e5b7d0694184534b
JS <souhrada@trebesin.cz>
2023-12-16 01:24:33 +0100

    teapot v1

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+import time
+import ascii_arts as asciis
+
+
+app = Flask(__name__)
+
+# following line is just for dev purposes
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+coffee_done = False
+
+
+@app.route("/")
+def index():
+    # return render_template("test.html"), 418
+    return render_template("teapot.html"), 418
+
+
+@app.route("/teapot", methods=["GET"])
+def teapot():
+    return teapot_default()
+
+
+@app.route("/teapot", methods=["BREW", "POST"])
+def brew():
+    return brew()
+
+
+@app.route("/teapot", methods=["WHEN"])
+def when():
+    return when()
+
+
+def teapot_default():
+    global coffee_done
+    if not coffee_done:
+        return asciis.send_chilling(), 418
+    elif coffee_done:
+        coffee_done = False
+        return asciis.send_coffee(), 418
+
+
+def brew():
+    return asciis.send_brewing(), 418
+
+
+def when():
+    global coffee_done
+    coffee_done = True
+    return asciis.send_pouring(), 418
